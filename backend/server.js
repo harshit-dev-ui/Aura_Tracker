e5bf39712f25ec49243141b5d6616e7bf213d1d3
Kanishkuu <kanishkkabra01@gmail.com>
2024-11-04 01:52:01 +0530

    backend initialize

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>console.log("MongoDB connected"))
+.catch(err=>console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
