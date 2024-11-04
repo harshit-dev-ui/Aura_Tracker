188e67639ad9d766fa78396ca502c2f14fb3437f
harshit-dev-ui <65815998+harshit-dev-ui@users.noreply.github.com>
2024-11-04 22:41:22 +0530

    layout for dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex h-screen space-between">
+      <div className="flex-1 bg-gray-200 flex items-center justify-center text-lg text-gray-700 m-4">
+        Container 1
+      </div>
+      <div className="flex-1 bg-gray-200 flex items-center justify-center text-lg text-gray-700 m-4">
+        Container 2
+      </div>
+      <div className="flex-1 bg-gray-200 flex items-center justify-center text-lg text-gray-700 m-4">
+        Container 3
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
