23ba65d774e318b0645d5c6fb9877690ac4568cf
Hashbrownsss <harshvardhanp157@gmail.com>
2024-11-05 02:56:25 +0530

    auth controller

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { login, signup, logout } from "../controller/auth.controller.js";
+import protectRoute from '../middleware/protectRoute.js';
+
+
+const router = express.Router();
+
+router.post('/login', login);
+router.post('/signup', signup);
+router.post('/logout',logout)
+
+router.get('/check-auth', protectRoute, (req, res) => {
+  res.json({ authenticated: true, user: req.user });
+});
+
+
+export default router;
